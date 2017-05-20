90d19de1907f5d1ce05d66f5d9120569831d3901
hansjuergnett <goeginett@hotmail.com>
2017-05-20 10:19:00 +0200

    Post hinzugefügt

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 'use strict'
 
 import express from 'express'
+import fs from 'fs'
+import bodyParser from 'body-parser'
+import cors from 'cors'
 let app = express()
+app.use(bodyParser.json())
+app.use(cors())
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/', function (req, res) {
-  res.send('hello world!!')
+app.get('/items', function (req, res) {
+  let data = fs.readFileSync('data.json','utf8')
+  res.send(data)
+})
+
+app.post('/items', function (req, res) {
+  let data = req.body
+  let content = fs.readFileSync('data.json','utf8')
+  let json = JSON.parse(content)
+  json.push(data)
+  fs.writeFileSync('data.json', JSON.stringify(json),'utf8')
+  res.json(data)
 })
 
 app.listen(3100)
